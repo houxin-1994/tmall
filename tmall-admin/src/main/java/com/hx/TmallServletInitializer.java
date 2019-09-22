b90a504551c257161ec07eff19c1bae9fa78803c
houxin <925812647@qq.com>
2019-09-22 18:32:32 +0800

    1、2019.9.22第一次提交tmall项目，已经用若依框架把架子搭好了。

---
+++ tmall-admin/src/main/java/com/hx/TmallServletInitializer.java
@@ -0,0 +1,18 @@
+package com.hx;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author houxin
+ */
+public class TmallServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(TmallApplication.class);
+    }
+}
